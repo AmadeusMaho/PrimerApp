b03cd7a28c8c87be8c80182b3f27ef16cc44d53d
AnnaHeisig <an.heisig@duocuc.cl>
2024-09-19 14:45:10 -0300

    login

--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -9,7 +9,21 @@ export class LoginPage implements OnInit {
 
   constructor() { }
 
+  
+
   ngOnInit() {
+  }
+  userInput=''
+  passwordInput=''
+  error=false
+
+  validar(){
+    if (this.userInput.trim()!='' && this.passwordInput.trim()!=''){
+      this.error=false;
+    }
+    else{
+      this.error=true;
+    }
   }
 
 }
