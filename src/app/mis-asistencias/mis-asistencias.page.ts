017d44bc9cb2581c71a69d7da68b1dcf504ad94e
Mauri <maur.gatica@duocuc.cl>
2024-09-19 22:23:02 -0300

    fecha y mi asistencia (tabla por mejorar)

--- src/app/mis-asistencias/mis-asistencias.page.ts
+++ src/app/mis-asistencias/mis-asistencias.page.ts
@@ -7,8 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MisAsistenciasPage implements OnInit {
 
-  constructor() { }
+asignatura_1 : string = 'Arquitectura'
+asignatura_2 : string = 'Estadística'
+asignatura_3 : string = 'Programación de aplicaciones móviles'
+asignatura_4 : string = 'Programación de Base de Datos'
+asignatura_5 : string = 'Proceso de Portafolio'
+asignatura_6 : string = 'Ética para el trabajo'
 
+fechaActual : string;
+mesActual : string;
+diaActualText : string;
+diaActual : string;
+anioActual : number;
+
+
+asistencias = [
+  {asignatura: 'Proceso de Portafolio', lastAttendance: '14-09-2024', estado: true},
+  {asignatura: 'Programación de Base de Datos', lastAttendance: '12-09-2024', estado: false},
+  {asignatura: 'Ética para el trabajo', lastAttendance: '07-09-2024', estado: true},
+  {asignatura: 'Programación de Base de Datos', lastAttendance: '05-09-2024', estado: true},
+  {asignatura: 'Estadística', lastAttendance: '19-09-2024', estado: true},
+  {asignatura: 'Programación de aplicaciones móviles', lastAttendance: '06-09-2024', estado: true},
+  {asignatura: 'Arquitectura', lastAttendance: '07-09-2024', estado: false},
+  {asignatura: 'Proceso de Portafolio', lastAttendance: '12-08-2024', estado: false}
+
+];
+  constructor() { 
+  this.fechaActual = new Date().toLocaleDateString('es-ES',{
+    day:'2-digit',
+    month: '2-digit',
+    year:'numeric'
+  })
+  this.anioActual = new Date().getFullYear();
+  this.diaActual = new Date().toLocaleDateString('es-ES',{
+    day:'2-digit'
+  })
+  this.mesActual = new Date().toLocaleDateString('es-ES', { month: 'long'});
+  this.diaActualText = new Date().toLocaleDateString('es-ES', { weekday: 'long'});
+}
   ngOnInit() {
   }
 
