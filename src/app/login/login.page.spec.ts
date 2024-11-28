f7763f6ec91332d91d41261794908b8c051ceddc
AnnaHeisig <an.heisig@duocuc.cl>
2024-11-28 18:59:39 -0300

    pruebas

--- src/app/login/login.page.spec.ts
+++ src/app/login/login.page.spec.ts
@@ -1,17 +1,45 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 import { LoginPage } from './login.page';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { ApirestService } from '../apirest.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('LoginPage', () => {
   let component: LoginPage;
   let fixture: ComponentFixture<LoginPage>;
+  let apirestService: ApirestService;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [LoginPage],
+      imports: [
+        IonicModule.forRoot(),
+        FormsModule,
+        HttpClientTestingModule
+      ],
+      providers: [ApirestService]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(LoginPage);
     component = fixture.componentInstance;
+    apirestService = TestBed.inject(ApirestService);
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Login funciona', fakeAsync( () => {
+    fixture.componentInstance.user.username = "Usuario1";
+    fixture.componentInstance.user.password = "MiClav3";
+
+    document.getElementById("btnValidar")?.click();
+    fixture.detectChanges();
+    tick();
+
+    const error = fixture.componentInstance.error
+    expect(error).toBeFalse()
+  }));
 });
--- src/app/regasistencia/regasistencia.page.spec.ts
+++ src/app/regasistencia/regasistencia.page.spec.ts
@@ -1,17 +1,36 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick, waitForAsync } from '@angular/core/testing';
 import { RegasistenciaPage } from './regasistencia.page';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import { ApirestService } from '../apirest.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
 
 describe('RegasistenciaPage', () => {
   let component: RegasistenciaPage;
   let fixture: ComponentFixture<RegasistenciaPage>;
+  let apirestService: ApirestService;
 
-  beforeEach(() => {
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [RegasistenciaPage],
+      imports: [
+        IonicModule.forRoot(),
+        FormsModule,
+        HttpClientTestingModule,
+        ActivatedRoute
+      ],
+      providers: [ApirestService]
+    }).compileComponents();
+
     fixture = TestBed.createComponent(RegasistenciaPage);
     component = fixture.componentInstance;
+    apirestService = TestBed.inject(ApirestService);
     fixture.detectChanges();
-  });
+  }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
 });
